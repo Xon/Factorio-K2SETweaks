e01ac3bb1e2346b81ea2fbd5c81985787e18b1a7
Xon <tazipper1@hotmail.com>
2020-08-03 15:46:33 +0800

    v0.0.2

---
+++ data-updates.lua
@@ -0,0 +1,9 @@
+tank1 = data.raw["storage-tank"]["kr-fluid-storage-1"]
+if tank1 then
+    tank1.se_allow_in_space = true
+end
+
+tank2 = data.raw["storage-tank"]["kr-fluid-storage-2"]
+if tank2 then
+    tank2.se_allow_in_space = true
+end
