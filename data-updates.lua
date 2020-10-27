426e4250b02ca81573cf2e2628c7bd9bb29f028c
Xon <tazipper1@hotmail.com>
2020-10-27 14:12:10 +0800

    Add krastorio 415/975 steam for electric boiler

--- data-updates.lua
+++ data-updates.lua
@@ -7,3 +7,6 @@ tank2 = data.raw["storage-tank"]["kr-fluid-storage-2"]
 if tank2 then
     tank2.se_allow_in_space = true
 end
+
+krastorio.technologies.addUnlockRecipe("se-electric-boiler", "se-electric-boiling-steam-415")
+krastorio.technologies.addUnlockRecipe("se-electric-boiler", "se-electric-boiling-steam-975")
--- data.lua
+++ data.lua
@@ -66,3 +66,45 @@ krastorio.matter_func.createMatterRecipe({
   unlocked_by_technology = "se-kr-matter-vitamelange-processing"
 })
 
+local data_util = require("__space-exploration__/data_util")
+
+local heat_capacity = data_util.string_to_number(data.raw.fluid.steam.heat_capacity)
+local boiler_power = 5000000
+local efficiency = 0.9
+
+data:extend({
+  {
+      type = "recipe",
+      name = "se-electric-boiling-steam-415",
+      results = {
+        { type = "fluid", name = "steam", amount = 100, temperature = 415 },
+      },
+      enabled = false,
+      energy_required = 2.5*(415-15) * 100 * heat_capacity / boiler_power / efficiency,
+      ingredients = {
+        { type = "fluid", name = "water", amount = 100 },
+      },
+      subgroup = "fluid-recipes",
+      requester_paste_multiplier = 1,
+      always_show_made_in = false,
+      category = "se-electric-boiling",
+      order = "a-a-b-c"
+  },
+  {
+      type = "recipe",
+      name = "se-electric-boiling-steam-975",
+      results = {
+        { type = "fluid", name = "steam", amount = 100, temperature = 975 },
+      },
+      enabled = false,
+      energy_required = 2.5*(975-15) * 100 * heat_capacity / boiler_power / efficiency,
+      ingredients = {
+        { type = "fluid", name = "water", amount = 100 },
+      },
+      subgroup = "fluid-recipes",
+      requester_paste_multiplier = 1,
+      always_show_made_in = false,
+      category = "se-electric-boiling",
+      order = "a-a-b-c"
+  }
+})
